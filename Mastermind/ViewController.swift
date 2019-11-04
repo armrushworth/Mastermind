7a690e2f598eabebaa45c3b65b629b4520a7b9d8
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-11-04 14:50:57 +0000

    Corrected cirlces to be buttons instead of just images

--- Mastermind/ViewController.swift
+++ Mastermind/ViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var inputSelection: UIView!
+    @IBOutlet weak var firstSelection: UIImageView!
+    @IBOutlet weak var secondSelection: UIImageView!
+    @IBOutlet weak var thirdSelection: UIImageView!
+    @IBOutlet weak var fourthSelection: UIImageView!
+    @IBAction func blueCircle(_ sender: Any) {
+    }
+    @IBAction func greenCircle(_ sender: Any) {
+    }
+    @IBAction func greyCircle(_ sender: Any) {
+    }
+    @IBAction func orangeCircle(_ sender: Any) {
+    }
+    @IBAction func redCircle(_ sender: Any) {
+    }
+    @IBAction func yellowCirlce(_ sender: Any) {
+    }
+    @IBAction func backButton(_ sender: Any) {
+    }
+    @IBAction func goButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         inputSelection.layer.borderWidth = 1
         inputSelection.layer.borderColor = UIColor.separator.cgColor
