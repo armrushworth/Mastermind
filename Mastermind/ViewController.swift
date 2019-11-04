9282131c4d819ffcd6ef4ea448dd2d395bf5367c
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-11-04 12:33:47 +0000

    Height of keyboard view adjusts according to screen width

--- Mastermind/ViewController.swift
+++ Mastermind/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var keyboardViewHeight: NSLayoutConstraint!
+    
     override func viewDidLoad() {
+        NotificationCenter.default.addObserver(self, selector: #selector(orientationChanged), name:  Notification.Name("UIDeviceOrientationDidChangeNotification"), object: nil)
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setWidth()
     }
-
-
+    
+    @objc func orientationChanged() {
+        setWidth()
+    }
+    
+    func setWidth() {
+        let screenRect = UIScreen.main.bounds
+        let screenWidth = screenRect.size.width
+        keyboardViewHeight.constant = screenWidth > 400 ? 100 : 200
+    }
 }
 
