b2f5f5a5c6084610675129146d5051db7c8a72b0
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-11-04 09:02:28 +0000

    Initial commit

---
+++ Mastermind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mastermind
+//
+//  Created by Rushworth, Ashley on 04/11/2019.
+//  Copyright © 2019 Rushworth, Ashley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
