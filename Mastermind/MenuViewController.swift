7abf686b53df7bc24af5a0bf09cf2b0e695e9ea3
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-11-06 17:02:09 +0000

    Added home and navigation between views

---
+++ Mastermind/MenuViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuViewController.swift
+//  Mastermind
+//
+//  Created by Rushworth, Ashley on 06/11/2019.
+//  Copyright © 2019 Rushworth, Ashley. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
